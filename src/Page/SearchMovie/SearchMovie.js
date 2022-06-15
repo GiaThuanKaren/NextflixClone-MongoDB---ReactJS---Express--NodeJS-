6a52ac7f8cbb8bf6d762e0e88b5d61405db377ef
GiaThuanKaren <GiaThuanNguyen213@gmail.com>
2022-06-15 13:06:59 +0700

    Add Pagination From Material UI Page Search 15/6/2022

--- src/Page/SearchMovie/SearchMovie.js
+++ src/Page/SearchMovie/SearchMovie.js
@@ -1,63 +1,65 @@
-import { useEffect, useState } from "react";
-import {useSelector} from "react-redux"
-import {Base_Url,ApiKey} from '../../Api/ApiConfig'
+import { useEffect, useState, ChangeEvent } from "react";
+import { useSelector } from "react-redux";
+import { Base_Url, ApiKey } from "../../Api/ApiConfig";
 import Footer from "../../Components/Footer/Footer";
 import { NavBar } from "../../Components/Header/Header";
 import MovieItem from "../../Components/MoveItem/MovieItem";
-import '../../grid.css'
-import './SearchMovie.css'
-function SearchMovie(){
-    const params = new URLSearchParams(location.search).get('query')
-    const CurState=useSelector((state)=>state);
-    const [Curpage,SetCurPage]=useState(1);
-    const [properties,Setproperties]=useState({
-        MaxPage:1,
-        CurFilm:[]
-    });
-    console.log(properties.CurFilm,properties.curPage)
-    const HandlePage=function(){
-        if(Curpage<properties.MaxPage)
-            SetCurPage(Curpage+1);
-    }
-    useEffect(()=>{
-        console.log("Fetching...")
-        // console.log(params.get('query'),params.get('data'))
-        fetch(`${Base_Url}/search/movie?${ApiKey}&query=${params}&page=${Curpage}`)
-            .then(res=>res.json())
-            .then(function(items){
-                console.log(items)
-                let tmp=properties.curPage+1;
-                    Setproperties({
-                        curPage:tmp,
-                        MaxPage:items.total_pages,
-                        CurFilm:[...properties.CurFilm,...items.results]
-                    })
-                // if(properties.curPage<properties.MaxPage){
-                //     console.log("Increasing..")
-                    
-                //     console.log("Curpage",tmp)
-                // }
-                
-                
-                
-            })
-    },[Curpage])
-    return(
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
+import "../../grid.css";
+import "./SearchMovie.css";
+
+function SearchMovie() {
+  const params = new URLSearchParams(location.search).get("query");
+  const CurState = useSelector((state) => state);
+  const [Curpage, SetCurPage] = useState(1);
+  const [properties, Setproperties] = useState({
+    MaxPage: 1,
+    CurFilm: [],
+  });
+  console.log(properties.CurFilm, properties.curPage);
+  const HandlePage = function (event, value) {
+    SetCurPage(value);
+    console.log(event, value);
+  };
+  useEffect(() => {
+    console.log("Fetching...");
+    // console.log(params.get('query'),params.get('data'))
+    fetch(`${Base_Url}/search/movie?${ApiKey}&query=${params}&page=${Curpage}`)
+      .then((res) => res.json())
+      .then(function (items) {
+        console.log(items);
+        let tmp = properties.curPage + 1;
+        Setproperties({
+          curPage: tmp,
+          MaxPage: items.total_pages,
+          CurFilm: [...items.results],
+        });
+      });
+  }, [Curpage]);
+  return (
     <>
-        <NavBar />
-        <div className="Main-contaner-Search-Page grid">
-            <div className="List-Movie-Search row">
-                {properties.CurFilm.map(function(item,idx){
-                    if(item.poster_path)
-                        return <MovieItem key={idx} item={item} Type='movie'/>
-                })}
-            </div>
-            <button
-                onClick={HandlePage}
-             >Load More</button>
+      <NavBar />
+      <div className="Main-contaner-Search-Page grid">
+        <div className="List-Movie-Search row">
+          {properties.CurFilm.map(function (item, idx) {
+            if (item.poster_path)
+              return <MovieItem key={idx} item={item} Type="movie" />;
+          })}
         </div>
-        <Footer />  
+        <div style={{display:"flex","justifyContent":"center",margin:"10px"}}>
+          <Stack>
+            <Pagination
+              count={properties.MaxPage}
+              page={Curpage}
+              color="secondary"
+              onChange={HandlePage}
+            />
+          </Stack>
+        </div>
+      </div>
+      <Footer />
     </>
-    )
+  );
 }
 export default SearchMovie;
